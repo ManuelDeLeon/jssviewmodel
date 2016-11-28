1fbc9457771c29b7d2e97daa5b4dd474873b0b98
ManuelDeLeon <manueldeleon@gmail.com>
2016-11-28 09:51:36 -0700

    update deferred message

--- ClientApp/components/Deferred/Deferred.js
+++ ClientApp/components/Deferred/Deferred.js
@@ -4,7 +4,10 @@ Deferred({
     <div class="row">
       <div class="col-md-4">
       <h1>Deferred Loading</h1>
-      <p>This component is loaded along with the rest of the app (main.js)</p>
+      <p>This component is loaded along with the rest of the app (main.js).</p>
+      <p>
+        This is particularly useful for landing pages. You can load above the fold content with SSR (minimal html) plus the component(s) to make it work (minimal js), and then load below the fold components asynchronously.
+      </p>
       <div style="margin-top: 50px">
         <Other b="defer: true" />
       </div>     
